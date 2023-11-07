52c967862fddae30d14c0228e74cd7cdbf506fcb
Mario <mariokozul24@gmail.com>
2023-11-07 11:27:29 +0100

    routes

--- routes/api.php
+++ routes/api.php
@@ -21,3 +21,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 */
 
 Route::post('/register', [AuthController::class, 'Register']);
+Route::post('/login', [AuthController::class, 'Login']);
+Route::middleware(['auth:api'])->group(function () {
+    Route::post('/logout', [AuthController::class,'Logout']);
+});
