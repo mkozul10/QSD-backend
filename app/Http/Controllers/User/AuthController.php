12b054b4e2d42c8d7022c50b30e662128baf3972
Mario <mariokozul24@gmail.com>
2023-11-09 00:40:30 +0100

    login function, mail handling and generating, saving and deleting validation keys

--- app/Http/Controllers/User/AuthController.php
+++ app/Http/Controllers/User/AuthController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
+use App\Mail\AuthMail;
 use App\Models\User;
 use Auth;
 use Illuminate\Http\Request;
@@ -11,9 +12,25 @@ use App\Http\Requests\LoginRequest;
 use DB;
 use Illuminate\Support\Facades\Hash;
 use Exception;
+use Illuminate\Support\Facades\Mail;
+use Carbon\Carbon;
 
 class AuthController extends Controller
 {
+    private function generateNumber(){
+        $min = 100000;
+        $max = 999999;
+
+        $randomNumber = rand($min, $max);
+        $randomNumber = str_pad($randomNumber, 6, '0', STR_PAD_LEFT);
+        return $randomNumber;
+    }
+
+    private function deleteKey($key){
+        DB::table('users_validation_keys')
+            ->where('validation_key', $key)
+            ->delete();
+    }
 
     public function Register(RegisterRequest $request){
         try{
@@ -37,35 +54,81 @@ class AuthController extends Controller
     }
 
     public function Login(LoginRequest $request){
-        try{
-            if(Auth::attempt($request->only("email","password"))){
-                $user = Auth::user();
-                $token = $user->createToken("qsdWebShop")->accessToken;
-                $user->role;
+        if(Auth::attempt($request->only("email","password"))){
+            $user = DB::table('users')
+                    ->where('email', '=', $request->email)
+                    ->get();
+            if($request->has('key')){
 
+                $key = (int)$request->key;
+                
+                $keyFromDB = DB::table('users_validation_keys')
+                    ->where('users_id', '=', $user[0]->id)
+                    ->select('validation_key','created_at')
+                    ->first();
+                if($keyFromDB !== null){
+                    if($key === $keyFromDB->validation_key){
+                        $createdAt = Carbon::parse($keyFromDB->created_at)->addHours(2);
+                        if(!$createdAt->isPast()){
+                            $this->deleteKey($keyFromDB->validation_key);
+
+                            if($user[0]->status){
+                                $user = Auth::user();
+                                $token = $user->createToken("qsdWebShop")->accessToken;
+                                $user->role;
+                                return response()->json([
+                                    'user' => $user,
+                                    'authorization' => [
+                                        'token' => $token,
+                                        'type' => 'Bearer',
+                                    ]
+                                ],200);
+                            }
+                        }
+                        else{
+                            $this->deleteKey($keyFromDB->validation_key);
+                            return response()->json([
+                                "error"=> "Validation key has expired."
+                            ],401);
+                        }
+                    } else {
+                        return response()->json([
+                            "error" => "Invalid validation key."
+                        ],400);
+                    }
+                }
+                else return response()->json(["error" => "Invalid validation key."],400);
+            } else {
+                $number = $this->generateNumber();
+                DB::table('users_validation_keys')
+                    ->insert([
+                        'validation_key' => $number,
+                        'users_id' => $user[0]->id,
+                        'created_at' => Carbon::now(),
+                        'updated_at' => Carbon::now()
+                    ]);
+                Mail::send('mail.validate',['number' => $number,'user' => $user], function ($message) use ($user) {
+                    $message->from('qsdshop@gmail.com', 'QSD WebShop')
+                        ->to($user[0]->email, $user[0]->name) 
+                        ->subject('QSD Verification code');
+                });
 
                 return response()->json([
-                    'user' => $user,
-                    'authorization' => [
-                        'token' => $token,
-                        'type' => 'bearer',
-                    ]
-                ],200);
+                    'message' => 'Verification code sent to your email'
+                ]);
             }
-        } catch(Exception $e){
+        } else {
             return response()->json([
-                'message' => $e->getMessage()
-            ],400);
+                "message" => "There was an error with your email or password. Please try again."
+            ],401);
         }
-        return response()->json([
-            'message' => 'Ivalid email or error'
-        ],401);
+        
     }
 
     public function Logout(Request $request)
 {
     $request->user()->token()->revoke();
-    return response()->json(['message' => 'Successfully logged out'], 400);
+    return response()->json(['message' => 'Successfully logged out'], 200);
 }
 
 }
---
+++ resources/views/mail/validate.blade.php
@@ -0,0 +1,4 @@
+Hi {{$user->name}}
+
+This is your validation key needed for login which is available for next 2 hours
+Validation key: {{ $number }}
