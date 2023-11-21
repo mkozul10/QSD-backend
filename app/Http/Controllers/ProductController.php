769eaf0e98e072e25d4d4e2cf890d5dc45676461
Mario <mariokozul24@gmail.com>
2023-11-21 23:15:09 +0100

    handler function for addProduct endpoint

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,24 +3,63 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\ProductRequest;
+use App\Models\Product;
+use App\Models\Image;
+use App\Models\ProductCategory;
+use App\Models\ProductSize;
 use Auth;
+use DB;
+use Carbon\Carbon;
 
 class ProductController extends Controller
 {
     public function addProduct(ProductRequest $request){
-        //file handling 
         $user = Auth::user();
+
         $files = $request->file('images');
-        $fileDetails = [];
+        $categories = $request->input('categories');
+        $sizes = $request->input('sizes');
+
+        $created = Product::create([
+            'name' => $request->input('name'),
+            'description'=> $request->input('description'),
+            'price'=> $request->input('price'),
+            'gender'=> $request->input('gender'),
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+            'colors_id' => (int) $request->input('brand_id'),
+            'brands_id' => (int) $request->input('color_id')
+        ]);
+        for( $i = 0; $i < count($categories); $i++ ){
+            ProductCategory::create([
+                'products_id' => $created->id,
+                'categories_id' => (int) $categories[$i]
+            ]);
+        }
+
+        for( $i = 0; $i < count($sizes); $i++ ){
+            ProductSize::create([
+                'products_id' => $created->id,
+                'sizes_id' => $sizes[$i]['size_id'],
+                'quantity' => $sizes[$i]['amount'],
+            ]);
+        }
+        
         foreach ($files as $file) {
             $uniqueFileName = uniqid("$user->name") . '.' . $file->getClientOriginalExtension();
             $file->storeAs('images', $uniqueFileName);
-            $fileDetails[] = [
+            Image::create([
                 'name' => $uniqueFileName,
-                'extension' => $file->getClientOriginalExtension(),
-                'path' => 'storage/images/' . $uniqueFileName
-            ];
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+                'products_id' => $created->id
+            ]);
         }
-        return response()->json($fileDetails);
+        $created->color;
+        $created->brand;
+        $created->images;
+        $created->categories;
+        $created->sizes;
+        return response()->json($created,200);
     }
 }
