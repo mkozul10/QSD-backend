10ee91a15f44ac28bb45b53afde8191a098a50c3
Mario <mariokozul24@gmail.com>
2023-11-25 01:42:54 +0100

    minor changes

--- app/Models/Image.php
+++ app/Models/Image.php
@@ -8,13 +8,21 @@ use Illuminate\Database\Eloquent\Model;
 class Image extends Model
 {
     use HasFactory;
+    public $timestamps = true;
     protected $table = 'images';
     protected $fillable = [
         'name',
         'created_at',
         'updated_at',
-        'products_id'
+        'products_id',
+        'link',
+        'hash'
     ];
+
+    protected $hidden = [
+        'hash'
+    ];
+    
     public function product(){
         return $this->belongsTo(Product::class,'products_id');
     }
