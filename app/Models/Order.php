6a21f0ddf9d939ffbb43d01ca6b9d7a7cb1d68a4
Mario <mariokozul24@gmail.com>
2023-11-28 20:09:13 +0100

    created model for Orders table and created relations

---
+++ app/Models/Order.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    use HasFactory;
+    public $timestamps = true;
+    protected $table = 'orders';
+    protected $fillable = [
+        'address',
+        'city',
+        'zip_code',
+        'phone',
+        'users_id',
+        'transaction_id',
+        'price',
+        'status',
+        'comment'
+    ];
+
+    protected $hidden = [
+        'comment',
+    ];
+
+    public function products(){
+        return $this->belongsToMany(ProductSize::class,'orders_products_sizes', 'orders_id', 'products_sizes_id')
+                    ->withPivot('quantity');
+    }
+
+    public function orderProductSizes(){
+        return $this->hasMany(OrderProductSizes::class,'orders_id');
+    }
+
+    public function user(){
+        return $this->belongsTo(User::class, 'users_id');
+    }
+}
