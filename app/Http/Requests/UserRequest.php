462a82c3f3c8e9632d87462936f63d47d002584d
Mario <mariokozul24@gmail.com>
2023-11-28 23:59:00 +0100

    minor changes

--- app/Http/Requests/UserRequest.php
+++ app/Http/Requests/UserRequest.php
@@ -4,7 +4,6 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Validation\Rule;
-use App\Http\Requests\UserController;
 
 class UserRequest extends FormRequest
 {
